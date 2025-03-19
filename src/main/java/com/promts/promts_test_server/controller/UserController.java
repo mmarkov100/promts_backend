5e236fa96aeda141ee8a1b9e26101a549cb36a49
mmarkov100 <pikola1epta@gmail.com>
2025-03-19 23:17:23 +0500

    Новый конроллер для фичи авторизации Auth

--- src/main/java/com/promts/promts_test_server/controller/UserController.java
+++ src/main/java/com/promts/promts_test_server/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.promts.promts_test_server.controller;
 
-import com.promts.promts_test_server.dto.Auth.inboind.EmailAndPasswordDTO;
 import com.promts.promts_test_server.dto.User.inbound.UpdateUserRequestDTO;
 import com.promts.promts_test_server.service.Auth.AuthService;
 import com.promts.promts_test_server.service.UserService;
@@ -21,32 +20,6 @@ public class UserController {
     private UserService userService;
     @Autowired
     private AuthService authService;
-
-    // Проверка актуальности токена. Нужен, чтобы при заходе на сайт если токен актуальный сразу переключалось на главный экран
-    //TODO Обратно поменять на гет запрос, а то нгрок хуета какая-то
-    @PostMapping("/tokencheck")
-    public ResponseEntity<?> tokenCheck(@RequestHeader String authorization) throws RuntimeException{
-
-        logger.info("Checking JWTToken.." + authorization);
-
-        return ResponseEntity.ok().body(authService.actualiseToken(authorization));
-    }
-
-    @PostMapping("/reg")
-    public ResponseEntity<?> regUser(@RequestBody EmailAndPasswordDTO emailAndPasswordDTO) throws RuntimeException{
-
-        logger.info("Registration new user..");
-
-        return ResponseEntity.ok().body(authService.registerNewUser(emailAndPasswordDTO.getEmail(), emailAndPasswordDTO.getPassword()));
-    }
-
-    @PostMapping("/log")
-    public ResponseEntity<?> logUser(@RequestBody EmailAndPasswordDTO emailAndPasswordDTO) throws RuntimeException{
-
-        logger.info("Logging user..");
-
-        return ResponseEntity.ok().body(authService.loginUserGetJWTToken(emailAndPasswordDTO.getEmail(), emailAndPasswordDTO.getPassword()));
-    }
 
     //TODO Обратно поменять на гет запрос, а то нгрок хуета какая-то
     @PostMapping
